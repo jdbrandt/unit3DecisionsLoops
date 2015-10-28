a56c3bb8210b9d452b2c717492df4987c27c4253
jdbrandt <jdbrandt@NN121-HA-18.HS.sd203.org>
2015-10-28 10:26:50 -0500

    Loops Pt. iI

---
+++ chap6practice/CompareAdjacentValues.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class CompareAdjacentValues
+{
+    public static final double EPSILON = 1e-12;
+    public static void checkAdjDups()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a series of numbers or a character to quit");
+        double input == 0;
+        while (s.hasNextDouble())
+        {
+            double previousInput = input;
+            input = s.nextDouble();
+            if (Math.abs(input-previousInput)<EPSILON)
+            {
+                System.out.println("Duplicate");
+                break;
+            }
+        }
+                
+            
+        
+    }
+}
